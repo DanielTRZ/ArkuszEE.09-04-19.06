da998e643bb92c572b0ff9381501da3ec8f520f2
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-05-05 20:42:36 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,101 @@
+// moim zdaniem najszybsze i najprostsze rozwiazanie skryptu zadania
+
+function dodawanie(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+   
+    
+ if( a ==""|| b ==""){
+       wynik.innerHTML="Prosze podać obie liczby.";
+ }
+   else  { 
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+     
+        var zliczenie = a + b;
+        wynik.innerHTML = "Wynik" +"  " + zliczenie;
+ }  
+   
+     }
+
+
+function odejmowanie(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+   
+    
+ if( a ==""|| b ==""){
+       wynik.innerHTML="Prosze podać obie liczby.";
+ }
+   else  { 
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+     
+        var zliczenie = a - b;
+        wynik.innerHTML = "Wynik" +"  " + zliczenie;
+ }  
+    
+    
+    
+}
+
+function mnozenie(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+   
+    
+ if( a ==""|| b ==""){
+       wynik.innerHTML="Prosze podać obie liczby.";
+ }
+   else  { 
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+     
+        var zliczenie = a * b;
+        wynik.innerHTML = "Wynik" +"  " + zliczenie;
+ }  
+    
+    
+    
+}function dzielenie(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+   
+    
+ if( a ==""|| b ==""){
+       wynik.innerHTML="Prosze podać obie liczby.";
+ }
+   else  { 
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+     
+        var zliczenie = a / b;
+        wynik.innerHTML = "Wynik" +"  " + zliczenie;
+ }  
+    
+    
+    
+}function potegowanie(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+   
+    
+ if( a ==""|| b ==""){
+       wynik.innerHTML="Prosze podać obie liczby.";
+ }
+   else  { 
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+     
+        var  zliczenie = a ** b;
+        wynik.innerHTML = "Wynik" +"  " + zliczenie;
+ }  
+    
+    
+    
+}
