2f18cf9b57dd5f93477d6fd1f505e862ef1bc525
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-08-29 08:48:03 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -13,7 +13,7 @@ a = parseFloat(a);
 b = parseFloat(b);
      
 var zliczenie = a + b;
-    wynik.innerHTML = "Wynik" +"  " + zliczenie;
+wynik.innerHTML = "Wynik" +"  " + zliczenie;
  }  
    
      }
