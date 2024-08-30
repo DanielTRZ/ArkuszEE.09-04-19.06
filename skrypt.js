6dcfca17baa614c8a7615140a2cdd82c44fe0643
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-08-30 08:40:59 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -21,7 +21,7 @@ function odejmowanie(){
 var a = document.getElementById("a").value;
 var b = document.getElementById("b").value;
      
- if( a ==""|| b ==""){
+if( a ==""|| b ==""){
        wynik.innerHTML="Prosze podać obie liczby.";
  }
    else  { 
