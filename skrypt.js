dbc187ae2ec64c5d652ea153c180d63d538e2c79
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-09-03 20:12:27 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -70,7 +70,7 @@ var b = document.getElementById("b").value;
    
     
  if( a ==""|| b ==""){
-       wynik.innerHTML="Prosze podać obie liczby.";
+    wynik.innerHTML="Prosze podać obie liczby.";
  }
    else  { 
         
